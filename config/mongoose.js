338652e9f3536d16a3f92a6616bbef1fbf0c9a99
Amit Vishwakarma <amit200698@gmail.com>
2023-06-04 18:45:04 +0530

    connect to new the server

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,8 @@
 // here we have call the mongoose to connect the database.
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://amit200698:g2UpuxXvgsVPvBwy@cluster0.ajgkki6.mongodb.net/todo-list');
+// mongoose.connect('mongodb+srv://amit200698:g2UpuxXvgsVPvBwy@cluster0.ajgkki6.mongodb.net/todo-list');
+mongoose.connect('mongodb+srv://admin:TOQCVCNz9bfa9BsG@cluster0.at7i2.mongodb.net/todo_devlopment');
 
 var db = mongoose.connection;
 
